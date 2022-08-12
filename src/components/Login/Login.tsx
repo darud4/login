34179697f6f8d3a9e99efa93ac5971c090f3debe
Pavel Bezrukov <darud4@yandex.ru>
2022-08-12 22:19:37 +0400

    feat: make login inputs controllable

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,30 @@
 import "./Login.css";
+import { useState, ChangeEvent } from "react";
 
 function Login() {
+  const [values, setValues] = useState({ username: "", password: "" });
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.currentTarget;
+//    console.log(name, value);
+    setValues({ ...values, [name]: value });
+  }
+
   return (
-    <form className="login" noValidate>
-      <input className="login__input" name="username" />
-      <input className="login__input" name="password" type="password" />
+    <form className="login" name="login" noValidate>
+      <input
+        onChange={handleChange}
+        className="login__input input-username"
+        name="username"
+        value={values.username}
+      />
+      <input
+        onChange={handleChange}
+        className="login__input input-password"
+        name="password"
+        type="password"
+        value={values.password}
+      />
     </form>
   );
 }
